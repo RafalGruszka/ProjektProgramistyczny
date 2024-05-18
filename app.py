c198dd30085fd18737c6780dd91ea76d09f00c7c
DawidFurs <dawid.furs@onet.pl>
2024-05-19 01:30:02 +0200

    Dodanie wykresu pogody wewnątrz aplikacji

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-# Aplikacja wspomagająca planowanie trekkingu lub wspinaczki górskiej
-# Autorzy: Rafał Gruszka, Dawid Furs
+import sys
 import json
-
-# Importowanie bibliotek PyQt6
-from PyQt6.QtCore import QDateTime, Qt, QTimer
-from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
-                             QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
-                             QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
-                             QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
-                             QVBoxLayout, QWidget, QTableWidgetItem)
+from PyQt6.QtCore import QDateTime, Qt
+from PyQt6.QtWidgets import (QApplication, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel,
+                             QLineEdit, QVBoxLayout, QPushButton, QRadioButton, QComboBox, QDateTimeEdit,
+                             QTableWidget, QTabWidget, QTextEdit, QSizePolicy, QTableWidgetItem, QWidget, QStyleFactory)
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from datetime import datetime
 import weatherComponents
 from openAIComponents import tripProposition
 
 # Parametry aplikacji
-version = 0.6                       # App version
-app_name = 'AI Trekking Advisor'    # App name
-app_width = 1100                    # App window width
-app_height = 600                    # App window height
+version = 0.6  # App version
+app_name = 'AI Trekking Advisor'  # App name
+app_width = 1100  # App window width
+app_height = 600  # App window height
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super().__init__(fig)
 
 
 class WidgetGallery(QDialog):
     def __init__(self, parent=None):
         super(WidgetGallery, self).__init__(parent)
-
-        trip_props_dict = {}  # Dictionary with trip propositions
+        self.trip_props_dict = {}  # Dictionary with trip propositions
 
         self.originalPalette = QApplication.palette()
         self.setMinimumHeight(app_height)
@@ -34,10 +38,7 @@ class WidgetGallery(QDialog):
 
         topLayout = QHBoxLayout()
         topLayout.heightForWidth(1)
-        #topLayout.addStretch(1)
-
         bottomLayout = QHBoxLayout()
-        #bottomLayout.addStretch(1)
 
         mainLayout = QVBoxLayout()
         mainLayout.addLayout(topLayout, 1)
@@ -46,10 +47,6 @@ class WidgetGallery(QDialog):
 
         mainLayout.addLayout(bottomLayout, 1)
         bottomLayout.addWidget(self.bottomTabWidget, 0)
-        #mainLayout.setRowStretch(1, 1)
-        #mainLayout.setRowStretch(2, 1)
-        #mainLayout.setColumnStretch(0, 1)
-        #mainLayout.setColumnStretch(1, 1)
         self.setLayout(mainLayout)
 
         self.setWindowTitle(f"{app_name} v. {str(version)}")
@@ -58,8 +55,6 @@ class WidgetGallery(QDialog):
     def changeStyle(self, styleName):
         QApplication.setStyle(QStyleFactory.create(styleName))
         QApplication.setPalette(QApplication.style().standardPalette())
-
-
 
     def createTopLeftGroupBox(self):
         self.topLeftGroupBox = QGroupBox("Aktywność")
@@ -101,69 +96,97 @@ class WidgetGallery(QDialog):
         layout.addWidget(styleComboBox)
         self.topRightGroupBox.setLayout(layout)
 
-
     def createbottomTabWidget(self):
         self.bottomTabWidget = QTabWidget()
-        self.bottomTabWidget.setSizePolicy(QSizePolicy.Policy.Preferred,
-                QSizePolicy.Policy.Ignored)
-        self.bottomTabWidget.setMinimumWidth(app_width-500)
-        tab1 = QTabWidget()
+        self.bottomTabWidget.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Ignored)
+        self.bottomTabWidget.setMinimumWidth(app_width - 500)
+
+        # Tab 1 - Table with places
+        tab1 = QWidget()
         tableWidget = QTableWidget(3, 3)
         tableWidget.setHorizontalHeaderLabels(["Miejsce", "Odległość w km", "Poziom trudności"])
         tableWidget.itemClicked.connect(self.tableItemClicked)
-
-
         tab1hbox = QHBoxLayout()
         tab1hbox.setContentsMargins(5, 5, 5, 5)
         tab1hbox.addWidget(tableWidget)
         tab1.setLayout(tab1hbox)
 
-        tab2 = QTabWidget() # Opis propozycji wyjazdu
+        # Tab 2 - Description of the trip
+        tab2 = QWidget()
         Tab2textEdit = QTextEdit()
-
-        Tab2textEdit.setPlainText("Propozycja trekkingu, wspinaczki lub innej aktywności w zależności od warunków atmosferycznych i lokalizacji.")
+        Tab2textEdit.setPlainText(
+            "Propozycja trekkingu, wspinaczki lub innej aktywności w zależności od warunków atmosferycznych i lokalizacji.")
         Tab2textEdit.setReadOnly(True)
         Tab2textEdit.setObjectName("Tab2textEdit")
-
-
         tab2hbox = QHBoxLayout()
         tab2hbox.setContentsMargins(5, 5, 5, 5)
         tab2hbox.addWidget(Tab2textEdit)
         tab2.setLayout(tab2hbox)
 
-        tab3 = QTabWidget()
+        # Tab 3 - Recommended equipment
+        tab3 = QWidget()
         Tab3textEdit = QTextEdit()
-
         Tab3textEdit.setPlainText("Lista zalecanego sprzętu w zależności od aktywności i warunków atmosferycznych.")
         Tab3textEdit.setReadOnly(True)
         Tab3textEdit.setObjectName("Tab3textEdit")
-
         tab3hbox = QHBoxLayout()
         tab3hbox.setContentsMargins(5, 5, 5, 5)
         tab3hbox.addWidget(Tab3textEdit)
         tab3.setLayout(tab3hbox)
 
+        # Tab 4 - Weather forecast
         tab4 = QWidget()
+        Tab4textEdit = QTextEdit()
+        Tab4textEdit.setPlainText("Pogoda.")
+        Tab4textEdit.setReadOnly(True)
+        Tab4textEdit.setObjectName("Tab4textEdit")
+        self.weatherCanvas = MplCanvas(self, width=5, height=4, dpi=100)
+        tab4hbox = QHBoxLayout()
+        tab4hbox.setContentsMargins(5, 5, 5, 5)
+        tab4hbox.addWidget(Tab4textEdit)
+        tab4hbox.addWidget(self.weatherCanvas)
+        tab4.setLayout(tab4hbox)
 
-        self.bottomTabWidget.addTab(tab1, "Lista &miejsc")
-        self.bottomTabWidget.addTab(tab2, "&Opis propozycji wyjazdu")
-        self.bottomTabWidget.addTab(tab3, "Lista zalecanego &sprzętu")
-        self.bottomTabWidget.addTab(tab4, "Prognoza &pogody")
+        self.bottomTabWidget.addTab(tab1, "Lista miejsc")
+        self.bottomTabWidget.addTab(tab2, "Opis propozycji wyjazdu")
+        self.bottomTabWidget.addTab(tab3, "Lista zalecanego sprzętu")
+        self.bottomTabWidget.addTab(tab4, "Prognoza pogody")
+
+    def draw_plot(self, weather_data):
+
+        hourly_temperatures = [hour['Temperature']['Value'] for hour in weather_data]
+        hours_list = [hour['DateTime'] for hour in weather_data]
+        hours_object = [datetime.fromisoformat(hour) for hour in hours_list]
+        hours = [hour.strftime("%H:%M") for hour in hours_object]
+        # Rysowanie wykresu na płótnie
+        self.weatherCanvas.axes.clear()
+        self.weatherCanvas.axes.plot(hours, hourly_temperatures, marker='o', linestyle='-')
+        self.weatherCanvas.axes.set_xlabel('Time (Hour)')
+        self.weatherCanvas.axes.set_ylabel('Temperature (°C)')
+        self.weatherCanvas.axes.set_title('Hourly Temperature Forecast')
+        self.weatherCanvas.axes.tick_params(axis='x', rotation=45)
+        self.weatherCanvas.axes.grid(True)
+        self.weatherCanvas.figure.tight_layout()
+        self.weatherCanvas.axes.set_title('Hourly Temperature Forecast')
+        self.weatherCanvas.draw()
 
     def tableItemClicked(self, item):
-
         place = self.bottomTabWidget.findChild(QTextEdit, "Tab2textEdit")
         equipment = self.bottomTabWidget.findChild(QTextEdit, "Tab3textEdit")
         placefromdict = self.trip_props_dict['trip_propositions'][item.row()]['proposition_details']
         equipmentfromdoct = self.trip_props_dict['trip_propositions'][item.row()]['equipment']
+        latitude = self.trip_props_dict['trip_propositions'][item.row()]['coordinates']['latitude']
+        longitude = self.trip_props_dict['trip_propositions'][item.row()]['coordinates']['longitude']
+        weather_data = weatherComponents.get_hourly_weather(latitude,longitude)
+
         place.setPlainText(placefromdict)
         equipment.setPlainText(equipmentfromdoct)
-
+        self.draw_plot(weather_data)
 
     def findLocation(self):
         lineEdit = self.topRightGroupBox.findChild(QLineEdit)
         place = lineEdit.text()
-        locationsWithId = weatherComponents.weatherLocations(place) # Get locationID from Accuweather API
+        locationsWithId = weatherComponents.weatherLocations(place)  # Get locationID from Accuweather API
 
         locations = []
         for i in range(len(locationsWithId)):
@@ -174,37 +197,28 @@ class WidgetGallery(QDialog):
         styleComboBox.clear()  # Clear combobox
         styleComboBox.addItems(locations)
 
-
-    def fillTableWidget(self, trip_proposition:dict):
+    def fillTableWidget(self, trip_proposition: dict):
         tableWidget = self.bottomTabWidget.findChild(QTableWidget)
 
         # get json attributes and set tableWidget items
         for prop in trip_proposition['trip_propositions']:
-            # print(prop)
             tableWidget.setItem(prop['number'] - 1, 0, QTableWidgetItem(prop['place']))
             tableWidget.item(prop['number'] - 1, 0).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-
             tableWidget.setItem(prop['number'] - 1, 1, QTableWidgetItem(str(prop['distance'])))
             tableWidget.item(prop['number'] - 1, 1).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-
             tableWidget.setItem(prop['number'] - 1, 2, QTableWidgetItem(str(prop['hardenes_level'])))
             tableWidget.item(prop['number'] - 1, 2).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
 
-            #print(str(prop['coordinates']['latitude']))
-            #print(str(prop['coordinates']['longitude']))
-
         tableWidget.resizeColumnsToContents()
-        #tableWidget.resizeRowsToContents()
 
     def proposeTrip(self):
-
         # get activity from radio button
         if self.topLeftGroupBox.findChild(QRadioButton).isChecked():
             activity = 'Trekking'
         else:
             activity = 'Wspinaczka'
 
-        # get location from lineEdit
+        # get location from combobox
         place = self.topRightGroupBox.findChild(QComboBox).currentText()
 
         trip_proposition = tripProposition(place, activity)
@@ -215,9 +229,6 @@ class WidgetGallery(QDialog):
 
 # Run app
 if __name__ == '__main__':
-
-    import sys
-
     app = QApplication(sys.argv)
     gallery = WidgetGallery()
     gallery.show()
--- weatherComponents.py
+++ weatherComponents.py
@@ -1,9 +1,15 @@
 import requests
+import matplotlib.pyplot as plt
+from datetime import datetime
 import json
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QTextEdit, QTabWidget, QTableWidget, QTableWidgetItem
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 apiKey = 'qWGdR3whkDhGy9dmO2AtwahlROfWH0ip'
 endpoint = 'https://dataservice.accuweather.com/locations/v1/'
-offline = False  # True - offline, False - online
+offline = True  # True - offline, False - online
 def weatherLocations(location: str) -> [str]:
 
 # Http request
@@ -15,7 +21,7 @@ def weatherLocations(location: str) -> [str]:
         return locations
     else:
         url = endpoint + '/search?apikey=' + apiKey + '&q=' + location + '&language=pl-pl&details=false&offset=20'
-        #print(url)
+        print(url)
         response = requests.get(url)
         json_data = json.loads(response.text)
 
@@ -24,4 +30,17 @@ def weatherLocations(location: str) -> [str]:
 
     return locations
 
-#print(weatherLocations('Jaworzno'))
+def get_hourly_weather(latitude, longitude):
+    api_key = "qWGdR3whkDhGy9dmO2AtwahlROfWH0ip"
+    url = f"https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey={api_key}&q={latitude},{longitude}"
+    response = requests.get(url)
+    location_data = response.json()
+    location_key = location_data['Key']
+
+    url_weather = f"https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/{location_key}?apikey={api_key}&metric=true"
+    response_weather = requests.get(url_weather)
+    weather_data = response_weather.json()
+    print(weather_data)
+    return weather_data
+
+
